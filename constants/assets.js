864c4eefbb7291d6cc71a22503357e4053759219
Chong Zhen Xi, Isaac <122247866+chonguschonguschongus@users.noreply.github.com>
2023-05-28 22:29:46 -0400

    Add files via upload

---
+++ constants/assets.js
@@ -0,0 +1,47 @@
+import badge from "../assets/icons/badge.png";
+import eth from "../assets/icons/eth.png";
+import heart from "../assets/icons/heart.png";
+import left from "../assets/icons/left.png";
+import search from "../assets/icons/search.png";
+import audience from "../assets/icons/audience.png";
+import star from "../assets/icons/star.png";
+import signupicon from "../assets/icons/signupicon.png";
+
+import logo from "../assets/images/logo.png";
+import event01 from "../assets/images/Court.jpg";
+import event02 from "../assets/images/ECP.jpg";
+import event03 from "../assets/images/Bungee.jpg";
+import event04 from "../assets/images/Cafe.jpg";
+import event05 from "../assets/images/Scuba.jpg";
+import event06 from "../assets/images/mahjong.jpg";
+import event07 from "../assets/images/Volleyball.jpg";
+
+import person01 from "../assets/images/person01.png";
+import person02 from "../assets/images/person02.png";
+import person03 from "../assets/images/person03.png";
+import person04 from "../assets/images/person04.png";
+
+export default {
+  badge,
+  eth,
+  heart,
+  left,
+  search,
+  audience,
+  star,
+  signupicon,
+
+  logo,
+  event01,
+  event02,
+  event03,
+  event04,
+  event05,
+  event06,
+  event07,
+
+  person01,
+  person02,
+  person03,
+  person04,
+};
