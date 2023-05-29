5c0292ea38950c8b058859c84ce7c7e210199893
Chong Zhen Xi, Isaac <122247866+chonguschonguschongus@users.noreply.github.com>
2023-05-28 22:29:34 -0400

    Add files via upload

---
+++ components/index.js
@@ -0,0 +1,23 @@
+import HomeHeader from "./HomeHeader";
+import EventCard from "./EventCard";
+import { RectButton, CircleButton } from "./Button";
+import { SubInfo, People, EndDate, Pax, EventTitle, Rating } from "./SubInfo";
+import DetailsBid from "./DetailsBid";
+import FocusedStatusBar from "./FocusedStatusBar";
+import DetailsDesc from "./DetailsDesc";
+
+export {
+  HomeHeader,
+  EventCard,
+  SubInfo,
+  People,
+  EndDate,
+  RectButton,
+  CircleButton,
+  Pax,
+  Rating,
+  EventTitle,
+  DetailsBid,
+  FocusedStatusBar,
+  DetailsDesc,
+};
